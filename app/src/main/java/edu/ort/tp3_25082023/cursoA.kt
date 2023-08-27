26a194eed2cea4d4586111afbd04dcfae686f2aa
piapotasznik <piapotasznik@gmail.com>
2023-08-27 18:09:14 -0300

    Initial commit

---
+++ app/src/main/java/edu/ort/tp3_25082023/cursoA.kt
@@ -0,0 +1,11 @@
+package edu.ort.tp3_25082023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cursoA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_curso)
+    }
+}
